4d549d560934732f6923af346498d7da85f2e50e
wallflower_vy <owoadevic@gmail.com>
2022-03-14 17:44:14 +0100

    Minor fix

---
+++ js/timer.js
@@ -0,0 +1,34 @@
+const dayEl=document.querySelector('.day');
+const hourEl=document.querySelector('.hour');
+const minutesEl=document.querySelector('.minute');
+const secondsEl=document.querySelector('.second');
+
+const currentTime =new Date();
+const currentYear =currentTime.getFullYear();
+const summitDay = new Date(`june 22 $ {currentYear + 1 } 00:00:00:00`);
+
+
+
+function updateTimer() {
+const  future = new Date(2022,05,22,00,00,00) , now = new Date(), diff = future - now;
+
+ const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+ const hours = Math.floor(diff / (1000 * 60 * 60));
+ const mins = Math.floor(diff / (1000 * 60));
+ const secs = Math.floor(diff / 1000);
+
+ const d = days < 0 ? 0 :  days;
+ const h = hours - days * 24;
+ const m = mins - hours * 60;
+ const s = secs - mins * 60;
+
+ dayEl.innerText = d
+ hourEl.innerText = h
+ minutesEl.innerText = m
+ secondsEl.innerText = s
+
+}
+setInterval('updateTimer()', 1000);
+
+
+// updateTimer();
